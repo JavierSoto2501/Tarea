0aa4d251d7df3ad0eefbc9b3c71942ce93dde5f6
Tomas <tomas.soza2501@gmail.com>
2026-04-17 17:15:12 -0400

    Metodo Intersectan

--- src/MovingObject.java
+++ src/MovingObject.java
@@ -71,7 +71,66 @@ public class MovingObject {
         }
         return distancia;
     }
-    public boolean Intersectan(MovingObject movingObject){return false;}
+    public boolean Intersectan(MovingObject movingObject){
+        if(this.cabeza==null || this.cabeza.siguiente==null || movingObject.cabeza==null || movingObject.cabeza.siguiente == null){
+            return false;
+        }
+        NodoTrayectoria tramoA = this.cabeza;
+        while (tramoA.siguiente!= null){
+            Trayectoria p1 = tramoA.trayectoria;
+            Trayectoria q1 = tramoA.siguiente.trayectoria;
+            NodoTrayectoria tramoB = movingObject.cabeza;
+            while (tramoB.siguiente!= null){
+                Trayectoria p2 = tramoB.trayectoria;
+                Trayectoria q2 = tramoB.siguiente.trayectoria;
+                int val1 = (q1.y - p1.y) * (p2.x - q1.x) - (q1.x - p1.x) * (p2.y - q1.y);
+                int o1 = 0;
+                if (val1 == 0) {
+                    o1 = 0;
+                } else if (val1 > 0) {
+                    o1 = 1;
+                } else {
+                    o1 = 2;
+                }
+                int val2 = (q1.y - p1.y) * (q2.x - q1.x) - (q1.x - p1.x) * (q2.y - q1.y);
+                int o2;
+                if (val2 == 0) {
+                    o2 = 0;
+                } else if (val2 > 0) {
+                    o2 = 1;
+                } else {
+                    o2 = 2;
+                }
+                int val3 = (q2.y - p2.y) * (p1.x - q2.x) - (q2.x - p2.x) * (p1.y - q2.y);
+                int o3;
+                if (val3 == 0) {
+                    o3 = 0;
+                } else if (val3 > 0) {
+                    o3 = 1;
+                } else {
+                    o3 = 2;
+                }
+
+                int val4 = (q2.y - p2.y) * (q1.x - q2.x) - (q2.x - p2.x) * (q1.y - q2.y);
+                int o4;
+                if (val4 == 0) {
+                    o4 = 0;
+                } else if (val4 > 0) {
+                    o4 = 1;
+                } else {
+                    o4 = 2;
+                }
+
+                if(o1 != o2 && o3 != o4){
+                    return true;
+                }
+                tramoB = tramoB.siguiente;
+
+            }
+            tramoA = tramoA.siguiente;
+        }
+        return false;
+    }
     public boolean IntersectaRangeST(Trayectoria rEsquina1, Trayectoria rEsquina2, int t1, int t2 ){
        int xMin, xMax, yMin, yMax;
 
